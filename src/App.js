c064302415220d0bf381fbd8601646438cc47535
vlad.legusha <vlad.legusha@gmail.com>
2020-08-26 14:52:33 +0300

    added routing,unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className={'btn-success'}>123</button>
-      </header>
+    <div className="App, mt-4">123
     </div>
   );
 }

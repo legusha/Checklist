e8aeb673bf5cb9b0475921100c2043fc80d0fe96
vlad.legusha <vlad.legusha@gmail.com>
2021-05-23 00:07:49 +0300

    fix responsive layout for note items

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -14,28 +14,36 @@ export default function CardList ({ list , view, action }) {
   const renderList = (list, view) => {
     const { render: renderView, helper: helperView } = view
 
-    return list.map(item => {
+    const renderList = list.map(item => {
       const { id, title } = item
       const cardBody = renderView(helperView(id))
 
       return (
-        <Card
-          key={id}
-          header={
-            <div className='d-flex align-center justify-between w-100'>
-              <h3 className='m-0 font-18 font-weight-5'>{title}</h3>
-              <div className='text-right'>
-                <IconPencil className='text-success cursor-point' onClick={() => handleIconAction(item, 'edit')}/>
-                <IconClose className='text-danger cursor-point' onClick={() => handleIconAction(item, 'delete')}/>
+        <div className='col-lg-3 col-md-6 col-12' key={id}>
+          <Card
+            classNameWrap={['card', 'mb-3', 'note', 'bg-white', 'h-100']}
+            header={
+              <div className='d-flex align-center justify-between w-100'>
+                <h3 className='m-0 font-18 font-weight-5'>{title}</h3>
+                <div className='text-right'>
+                  <IconPencil className='text-success cursor-point' onClick={() => handleIconAction(item, 'edit')}/>
+                  <IconClose className='text-danger cursor-point' onClick={() => handleIconAction(item, 'delete')}/>
+                </div>
               </div>
-            </div>
-          }
-          body={
-            cardBody
-          }
-        />
+            }
+            body={
+              cardBody
+            }
+          />
+        </div>
       )
     })
+
+    return (
+      <div className='row w-100'>
+        {renderList}
+      </div>
+    )
   }
   return (
     <div className={'d-flex justify-between flex-wrap notes-list p-4 border mt-4'}>

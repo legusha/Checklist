eb245d0b85abb02eca4b417adfa7e289e38791b7
vlad.legusha <vlad.legusha@gmail.com>
2021-06-07 20:42:10 +0300

    update path in app

--- config-overrides.js
+++ config-overrides.js
@@ -2,9 +2,7 @@ const {alias} = require('react-app-rewire-alias')
 
 module.exports = function override(config) {
   alias({
-    '@components': 'src/components',
-    '@assets' : 'src/assets',
-    '@views' : 'src/views'
+    '@': 'src',
   })(config)
 
   return config
